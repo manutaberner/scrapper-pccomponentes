a7182c07f78304e61cddd0bc33c764e09fab25b9
magnumelio <manutabernerllorca@gmail.com>
2020-10-12 20:39:48 +0200

    It seems to work as intended
    The key is in adjusting the variable SCROLL speed according to observe if the script loads the ALL the items of the webpage

--- scrapper-pccom-all.py
+++ scrapper-pccom-all.py
@@ -4,18 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 import pandas as pd
 import time
-
+start = time.time()
 #Previously download the DRIVER depending on which BROWSER are you going to use
 #in this case I am using chrome
 #download chrome driver depending on your borwser VERSION
 #download page https://chromedriver.chromium.org/downloads
 driver = webdriver.Chrome('/Users/manutaberner/Downloads/chromedriver') #path to driver
 
-product_list = []
-price_list = []
-brand_list = []
-category_list = []
+product_list = [] #list of the product names
+price_list = [] #list of the product prices
+brand_list = [] #list of the product brands
+category_list = [] #list of the prudct category
 
+#List of the components websites
 components_list = ['placas-base',
                     'procesadores',
                     'tarjetas-graficas',
@@ -31,6 +32,7 @@ components_list = ['placas-base',
                     'cables-internos-de-pc',
                     'conectividad']
 
+#STARTS to navigate trough websites
 for com in range(len(components_list)):
     #getting the web page
     webpage = 'https://www.pccomponentes.com/'+components_list[com]
@@ -42,13 +44,13 @@ for com in range(len(components_list)):
 
     #SCROLL TO BOTTOM to load all the content
     current_scroll_position, new_height= 0, 1 #start position and height of the scroll
-    scroll_speed = 30 #SPEED of the scroll, depends on YOUR INTERNET, if your time of page loading is slow DECREASE the number
+    scroll_speed = 10 #SPEED of the scroll, depends on YOUR INTERNET, if your time of page loading is slow DECREASE the number
 
     #starts scrolling
     while current_scroll_position <= new_height:
         current_scroll_position += scroll_speed
         driver.execute_script("window.scrollTo(0, {});".format(current_scroll_position))
-        time.sleep(0.1) #time to let the page load
+        time.sleep(0.01) #time to let the page load
         new_height = driver.execute_script("return document.body.scrollHeight")
 
     #GETTING THE PRODUCT INFORMATION
@@ -57,7 +59,7 @@ for com in range(len(components_list)):
     brands = driver.find_elements_by_xpath("//a[@class='c-product-card__title-link cy-product-link']")    
     categories = driver.find_elements_by_xpath("//a[@class='c-product-card__title-link cy-product-link']")    
 
-
+    rows = 0
     for p in range(len(names)):
         product_list.append(names[p].text)
         precio = prices[p].get_attribute("data-price")
@@ -65,12 +67,18 @@ for com in range(len(components_list)):
         marca = brands[p].get_attribute("data-brand")
         brand_list.append(marca)
         categoria = categories[p].get_attribute("data-category")
-        category_list.append(categoria)    
+        category_list.append(categoria)
+        rows += 1
+
+    #debugger information
+    print('Total ROWS: '+str(rows)+' '+str(categoria))
+
 
 #CREATING THE DATAFRAME
 data_tuples = list(zip(product_list[0:],price_list[0:],brand_list[0:],category_list[0:]))
 df = pd.DataFrame(data_tuples, columns=['Product','Price','Brand','Category'])
 
+print()
 print(df)
 
 #Output to CSV format
@@ -78,5 +86,13 @@ now = datetime.now() # current date and time
 date_time = now.strftime("%m/%d/%Y_%H:%M:%S")
 df.to_csv('pccom-componentes_.csv',index = False, encoding='utf-8')
 
-#close the driver
+#total time of execution of the script
+end = time.time()
+total_time = end - start
+total_time = str(round(total_time,2))
+print()
+print('Time of the SCRIPT execution: '+ total_time +' seconds')
+print()
+
+#close the driver and the browser gets closed
 driver.close()
